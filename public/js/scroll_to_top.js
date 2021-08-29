cf2d57bd5a84c714fdbb91c88969de1f7e6e56f7
Nikolai-415 <nikolai-415@yandex.ru>
2021-08-29 06:23:41 +0300

    Добавил прокручивание к форме фильтрации и сортировки; Добавил кнопку "Наверх" на сайте

---
+++ public/js/scroll_to_top.js
@@ -0,0 +1,21 @@
+// Получение объекта кнопки
+buttonScrollToTop = document.getElementById("buttonScrollToTop");
+
+// Вызов функции проверки при каждом прокручивании страницы
+window.onscroll = function() { scrollFunction() };
+
+// Функция проверки - если страница прокручена ниже 20 пикселей, то кнопка видна, иначе - нет
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        buttonScrollToTop.style.display = "block";
+    }
+    else {
+        buttonScrollToTop.style.display = "none";
+    }
+}
+
+// Функция прокручивания при нажатии на кнопку
+function scrollToTopFunction() {
+    document.body.scrollTop = 0; // Для Safari
+    document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
